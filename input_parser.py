5459574f3fd6a6943e8d63c79e4c356329241670
rodrigo1110 <62890245+rodrigo1110@users.noreply.github.com>
2022-01-18 16:11:30 +0000

    Add files via upload

---
+++ input_parser.py
@@ -0,0 +1,5 @@
+#python tool(Soon)
+
+
+#1-parsing of both json inputs and creation of AST
+#2-transversing AST, definition of rules of thumb, implementation of algorithm based on those rules, and json ouptut creation in other future files 
